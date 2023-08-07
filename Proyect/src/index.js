fcc000b951935b80161402b405b8e31db82f961b
Bryan España <esp21550@uvg.edu.gt>
2023-08-07 08:16:35 -0600

    Implementación de estrellas y fondo

--- Proyect/src/index.js
+++ Proyect/src/index.js
@@ -71,8 +71,69 @@ scene.add(light);
 camera.position.set (0, 0, 200);
 camera.lookAt(cube.position);
 
+
+
+const starCount = 100000; 
+const starGeometry = new THREE.BufferGeometry();
+
+// Carga la textura
+const textureLoader = new THREE.TextureLoader();
+const starTexture = textureLoader.load('src/assets/img/star.png'); // Reemplaza 'ruta_a_tu_textura.png' con la ruta correcta a tu archivo de textura
+
+// Modifica el material para usar la textura
+const starMaterial = new THREE.PointsMaterial({ 
+    color: 0xFFFFFF, 
+    size: 2.5,
+    map: starTexture,
+    transparent: true, 
+    blending: THREE.NormalBlending
+});
+
+const positions = new Float32Array(starCount * 3);
+for (let i = 0; i < starCount * 3; i++) {
+    positions[i] = (Math.random() - 0.5) * 2000;
+}
+starGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
+
+const starField = new THREE.Points(starGeometry, starMaterial);
+scene.add(starField);
+
+
+const starCountNear = 50000;
+const starCountFar = 50000;
+
+// Geometría y material para el campo de estrellas cercano
+const starGeometryNear = new THREE.BufferGeometry();
+const positionsNear = new Float32Array(starCountNear * 3);
+for (let i = 0; i < starCountNear * 3; i++) {
+    positionsNear[i] = (Math.random() - 0.5) * 1000; // espacio más pequeño para estrellas cercanas
+}
+starGeometryNear.setAttribute('position', new THREE.BufferAttribute(positionsNear, 3));
+const starFieldNear = new THREE.Points(starGeometryNear, starMaterial);
+scene.add(starFieldNear);
+
+// Geometría y material para el campo de estrellas lejano
+const starGeometryFar = new THREE.BufferGeometry();
+const positionsFar = new Float32Array(starCountFar * 3);
+for (let i = 0; i < starCountFar * 3; i++) {
+    positionsFar[i] = (Math.random() - 0.5) * 3000; // espacio más grande para estrellas lejanas
+}
+starGeometryFar.setAttribute('position', new THREE.BufferAttribute(positionsFar, 3));
+const starFieldFar = new THREE.Points(starGeometryFar, starMaterial);
+scene.add(starFieldFar);
+
 //renderer.render(scene, camera);
 loopMachine.addCallback(() => {
+
+
+    starFieldNear.position.x += 0.05; // Mueve las estrellas cercanas más rápido
+    starFieldFar.position.x += 0.02;  // Mueve las estrellas lejanas más lento
+
+    // Restablece la posición si las estrellas se han movido demasiado
+    if (starFieldNear.position.x > 1000) starFieldNear.position.x = -1000;
+    if (starFieldFar.position.x > 3000) starFieldFar.position.x = -3000;
+
+
     if (planetModelGlobal) { // solo rota si el modelo ya fue cargado y asignado
         planetModelGlobal.rotation.y += 0.002; // 3. Añade la rotación al modelo en el callback
     }
