6c5d3299958ac9937c5060b4031b48c12ea7ad70
Bryan España <bryanespana21550@gmail.com>
2023-10-05 17:28:06 -0600

    Implementación de exoplanetas

--- Proyect/src/utils/threejs/light.js
+++ Proyect/src/utils/threejs/light.js
@@ -1,5 +1,5 @@
-const light = new THREE.AmbientLight(0xffffff, 3); // Luz ambiental blanca pero con menor intensidad (0.2 en este caso)
-const directionalLight = new THREE.DirectionalLight(0xFF0000);  // Rojo puro para la luz direccional
+const light = new THREE.AmbientLight(0xFFFFFF, 2); // Luz ambiental blanca pero con menor intensidad (0.2 en este caso)
+const directionalLight = new THREE.DirectionalLight(0xFFFFFF);  // Rojo puro para la luz direccional
 directionalLight.position.set(-10, 10, 10);
 light.add(directionalLight);
 export default light;
