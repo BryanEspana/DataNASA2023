ea552125b4d6b7b4f38c438d7269de7b951e6999
Bryan España <bryanespana21550@gmail.com>
2023-10-05 21:21:18 -0600

    Agregar archivo vite config

---
+++ Proyect/vite.config.js
@@ -0,0 +1,9 @@
+// vite.config.js
+export default {
+    build: {
+      rollupOptions: {
+        input: 'src/components/galerycomponent/galery.html',  // Asegúrate de que la ruta a gallery.html sea correcta
+      },
+    },
+  }
+  
